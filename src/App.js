1b61a00aa49d3bf918337377647e5c24674b93b5
Harsh Virani <iamharshvirani7@gmail.com>
2020-08-05 18:26:54 -0700

    LineChart & Added CountryPicker

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { fetchData } from './api';
 class App extends React.Component {
     state = {
         data: {},
+        country: '',
     }
 
     async componentDidMount() {
@@ -19,8 +20,8 @@ class App extends React.Component {
         return (
             <div className={styles.container}>
                 <Cards data={data} />
-                <Chart />
                 <CountryPicker />
+                <Chart />
             </div>
         )
     }
